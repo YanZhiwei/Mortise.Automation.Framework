fe7c2632c9971d3cde1cd64200e8474cf82c1240
YanZhiwei <Yan.Zhiwei@outlook.com>
2024-06-15 22:43:32 +0800

    增加Configurations&Extensions机制

---
+++ src/UIA/Mortise.UIAAccessibility/Configurations/UiaAccessibleOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Mortise.UiaAccessibility.Configurations;
+
+public sealed class UiaAccessibleOptions
+{
+    internal IList<IUiaAccessibleOptionsExtension> AccessibleOptions { get; } =
+        new List<IUiaAccessibleOptionsExtension>();
+
+    public void RegisterAccessible(IUiaAccessibleOptionsExtension accessibleOptionsExtension)
+    {
+        if (accessibleOptionsExtension == null)
+            throw new ArgumentNullException(nameof(accessibleOptionsExtension));
+
+        AccessibleOptions.Add(accessibleOptionsExtension);
+    }
+}
---
+++ src/UIA/Mortise.UiaAccessibility.WeChat/Configurations/WeChatAccessibleOptions.cs
@@ -0,0 +1,15 @@
+﻿using Mortise.UiaAccessibility.Configurations;
+using Mortise.UiaAccessibility.WeChat.Extensions;
+
+namespace Mortise.UiaAccessibility.WeChat.Configurations;
+
+public static class WeChatAccessibleOptions
+{
+    public static UiaAccessibleOptions AddWeChatAccessible(this UiaAccessibleOptions options)
+    {
+        if (options == null)
+            throw new ArgumentNullException(nameof(options));
+        options.RegisterAccessible(new WeChatAccessibleOptionsExtension(options));
+        return options;
+    }
+}
