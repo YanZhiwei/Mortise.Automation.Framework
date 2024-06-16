b6ad90cb4b5d31967b9d319ca99ed1e9adb49601
YanZhiwei <Yan.Zhiwei@outlook.com>
2024-06-16 10:44:07 +0800

    增加Mortise.UiaAccessibilityTests单元测试

---
+++ test/Mortise.UiaAccessibilityTests/UiaAccessibleTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Mortise.Accessibility.Abstractions;
+using Mortise.UiaAccessibility.Extensions;
+using Mortise.UiaAccessibility.WeChat.Configurations;
+
+namespace Mortise.UiaAccessibilityTests;
+
+[TestClass]
+public class UiaAccessibleTests
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public UiaAccessibleTests()
+    {
+        _serviceProvider = new ServiceCollection()
+            .AddLogging(loggingBuilder => loggingBuilder.SetMinimumLevel(LogLevel.Debug))
+            .AddUiaAccessible(option => { option.AddWeChatAccessible(); }).BuildServiceProvider();
+    }
+
+    [TestMethod]
+    public void UiaAccessibleTest()
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var uiaAccessible = scope.ServiceProvider.GetService<Accessible>();
+            Assert.IsNotNull(uiaAccessible);
+        }
+    }
+}
