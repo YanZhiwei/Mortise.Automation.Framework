17fd4907386fc639e2dc59bbd1b12bcf6d291a24
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-06-20 00:24:21 +0800

    JsonAccessibleLocatorStorage初步实现 (#10)

---
+++ src/Mortise.Accessibility.Locator.Json/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Mortise.Accessibility.Locator.Abstractions;
+using Tenon.Serialization.Json.Extensions;
+
+namespace Mortise.Accessibility.Locator.Json.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddJsonLocatorStorage(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IAccessibleLocatorStorage, JsonAccessibleLocatorStorage>();
+        services.AddSystemTextJsonSerializer();
+        return services;
+    }
+}
