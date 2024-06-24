68a02d1506385d1fb2266b9f34db26dd85438542
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-06-24 23:45:57 +0800

    调整IAccessibleLocatorStorage定义以及实现 (#13)

--- src/Mortise.Accessibility.Locator.Json/Configurations/JsonLocatorStorageOptions.cs
+++ src/Mortise.Accessibility.Locator.Json/Configurations/JsonLocatorStorageOptions.cs
@@ -5,7 +5,7 @@ namespace Mortise.Accessibility.Locator.Json.Configurations;
 
 public sealed class JsonLocatorStorageOptions : AccessibleLocatorOptions
 {
-    public string AppData { get; set; } = Path.Combine(AppContext.BaseDirectory, "locators");
+    public string AppData { get; set; } = Path.Combine(AppContext.BaseDirectory, ".locators");
 
     public Encoding Encoding { get; set; } = Encoding.UTF8;
 }
