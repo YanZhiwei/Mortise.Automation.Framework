f70f3251f255fd590f71465cbff94aa0326a26f5
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-07-14 22:48:37 +0800

    新增Mortise.Platform定义 (#19)

---
+++ src/Mortise.Platform.Windows/WindowsSystemInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using Tenon.Infra.Windows.Win32;
+
+namespace Mortise.Platform.Windows;
+
+public class WindowsSystemInteraction : ISystemInteraction
+{
+    public IntPtr GetWindowHandle(Point point = default)
+    {
+        return point.IsEmpty ? IntPtr.Zero : Window.Get(point);
+    }
+}
---
+++ src/Mortise.Platform/ISystemInteraction.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Mortise.Platform;
+
+public interface ISystemInteraction
+{
+    /// <summary>
+    ///     根据坐标获取窗口句柄
+    /// </summary>
+    /// <param name="point">Point</param>
+    /// <returns>窗口句柄</returns>
+    public IntPtr GetWindowHandle(Point point = default);
+}
