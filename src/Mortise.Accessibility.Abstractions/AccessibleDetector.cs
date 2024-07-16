5e76f56005c3dcbc501969ccf7dad2a3c0028565
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-07-16 21:16:39 +0800

    新增FromPointAsync 定义 (#20)

--- src/Mortise.Accessibility.Abstractions/AccessibleDetector.cs
+++ src/Mortise.Accessibility.Abstractions/AccessibleDetector.cs
@@ -7,5 +7,7 @@ public abstract class AccessibleDetector
     public string ProcessName { get; protected set; } = "*";
     public AccessiblePriority Priority { get; protected set; } = AccessiblePriority.Normal;
     public abstract AccessibleComponent? FromPoint(Point location);
+
+    public abstract Task<AccessibleComponent?> FromPointAsync(Point location);
     public abstract AccessibleComponent? DtoAccessibleComponent(object element, Accessible? accessibility = null);
 }
