1bcde2222f7b9ad65d2e9c511714ea4db8760d6e
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-07-30 23:51:30 +0800

    完善AccessibleControlType定义

---
+++ src/Browser/Mortise.BrowserAccessibility/Models/Dom.cs
@@ -0,0 +1,14 @@
+﻿namespace Mortise.BrowserAccessibility.Models;
+
+public sealed class Dom
+{
+    public string CustomId { get; set; }
+    public string TagName { get; set; }
+    public int IframeIndex { get; set; } = -1;
+    public DomRect? BoundingRect { get; set; }
+
+    public override string ToString()
+    {
+        return $"{TagName} {CustomId}";
+    }
+}
